6750dbc8c3cac121ae5b0d0f7180ff2973f11f7e
blurryxd <20337853+blurryxd@users.noreply.github.com>
2018-11-15 12:59:18 +0200

    Add files via upload

--- Tiedot.sql
+++ Tiedot.sql
@@ -1,8 +1,12 @@
-CREATE TABLE Sivut
+CREATE TABLE Kayttajat
 (
-  Profiili INT NOT NULL,
-  Käyttäjät INT NOT NULL,
-  PRIMARY KEY (Profiili)
+  ID INT NOT NULL,
+  Kayttajanimi INT NOT NULL,
+  Sivu INT NOT NULL,
+  Salasana INT NOT NULL,
+  PRIMARY KEY (ID),
+  UNIQUE (Kayttajanimi),
+  UNIQUE (Sivu)
 );
 
 CREATE TABLE Kommentit
@@ -10,16 +14,6 @@ CREATE TABLE Kommentit
   KuvaID INT NOT NULL,
   Kommentti INT NOT NULL,
   PRIMARY KEY (KuvaID)
-);
-
-CREATE TABLE Kayttajat
-(
-  ID INT NOT NULL,
-  Kayttajanimi INT NOT NULL,
-  Profiili INT NOT NULL,
-  PRIMARY KEY (ID),
-  FOREIGN KEY (Profiili) REFERENCES Sivut(Profiili),
-  UNIQUE (Kayttajanimi)
 );
 
 CREATE TABLE Kuvat
@@ -37,4 +31,3 @@ CREATE TABLE Kuvat
   UNIQUE (KuvaID),
   UNIQUE (Kuvalocation)
 );
-
