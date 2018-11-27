54506305f64fac3d3339791d290d646de872ceb0
blurryxd <20337853+blurryxd@users.noreply.github.com>
2018-11-27 11:42:06 +0200

    Update Tiedot.sql

--- Tiedot.sql
+++ Tiedot.sql
@@ -1,33 +1,56 @@
+ ERDPlus
+
+    FAQ
+
+    The textbook using ERDPlus is available.
+
+    Need professional database modeling?
+
+    Juho Huhtanen
+    Diagrams
+    Logout
+
+Export SQL
+Beta: Please email problems, questions, or suggestions to info@erdplus.com.
+
 CREATE TABLE Kayttajat
 (
   ID INT NOT NULL,
   Kayttajanimi INT NOT NULL,
-  Sivu INT NOT NULL,
   Salasana INT NOT NULL,
+  profiilikuva INT NOT NULL,
   PRIMARY KEY (ID),
-  UNIQUE (Kayttajanimi),
-  UNIQUE (Sivu)
-);
-
-CREATE TABLE Kommentit
-(
-  KuvaID INT NOT NULL,
-  Kommentti INT NOT NULL,
-  PRIMARY KEY (KuvaID)
+  FOREIGN KEY (profiilikuva) REFERENCES Kuvat(kuvaID),
+  UNIQUE (Kayttajanimi)
 );
 
 CREATE TABLE Kuvat
 (
-  ID INT NOT NULL,
-  KuvaID INT NOT NULL,
-  Profilepic INT NOT NULL,
-  Tykkaukset INT NOT NULL,
   Kuvalocation INT NOT NULL,
-  ID INT NOT NULL,
-  KuvaID INT NOT NULL,
-  PRIMARY KEY (ID),
-  FOREIGN KEY (ID) REFERENCES Kayttajat(ID),
-  FOREIGN KEY (KuvaID) REFERENCES Kommentit(KuvaID),
-  UNIQUE (KuvaID),
-  UNIQUE (Kuvalocation)
+  kuvaID INT NOT NULL,
+  date DATE NOT NULL,
+  Nayttokerrat INT NOT NULL,
+  Kayttaja INT NOT NULL,
+  PRIMARY KEY (kuvaID),
+  FOREIGN KEY (Kayttaja) REFERENCES Kayttajat(ID)
 );
+
+CREATE TABLE Kommentit
+(
+  Kommentti INT NOT NULL,
+  date DATE NOT NULL,
+  kuvaID INT NOT NULL,
+  Kayttaja INT NOT NULL,
+  FOREIGN KEY (kuvaID) REFERENCES Kuvat(kuvaID),
+  FOREIGN KEY (Kayttaja) REFERENCES Kayttajat(ID)
+);
+
+CREATE TABLE Tykkaykset
+(
+  boolean INT NOT NULL,
+  kuvaID INT NOT NULL,
+  Kayttaja INT NOT NULL,
+  FOREIGN KEY (kuvaID) REFERENCES Kuvat(kuvaID),
+  FOREIGN KEY (Kayttaja) REFERENCES Kayttajat(ID)
+);
+
